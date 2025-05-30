7993bbdc26d0435dadedeb59fecefac858de7e00
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-05-29 21:19:56 -0400

    styled movie card component
    Added scale and zoom to hover
    
    background and shadow to heart

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -20,20 +20,35 @@ function MovieCard({ movie }: MovieCardProps) {
     else addToFavourites(movie);
   }
   return (
-    <div className="flex flex-col items-center p-4 my-8 bg-gray-100 text-black rounded-lg w-full hover:opacity-80 transition-opacity duration-300 shadow-lg">
-      <div className="rounded-sm">
+    <div className="group relative center my-4 bg-gray-100 text-black rounded-lg w-full aspect-[2/3] hover:opacity-80 transition-opacity duration-300 shadow-lg hover:scale-105 transition-transform duration-300">
+      {/* Image Background */}
+      <div className="absolute inset-0 overflow-hidden rounded-lg">
         <img
-          className="rounded-lg"
+          className="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-110"
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
-        <div>
-          <FavouriteButton onClick={onFavouriteClick} favourited={favourite} />
-        </div>
+        \
       </div>
-      <div>
-        <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+      {/* Foreground Content (text and heart) */}
+      <div className="relative z-10 flex flex-col justify-between h-full w-full p-4 text-white">
+        <div className="flex justify-end">
+          <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <FavouriteButton
+              onClick={onFavouriteClick}
+              favourited={favourite}
+              className={
+                "shadow-xl bg-white bg-opacity-20 backdrop-blur-sm rounded-full p-2"
+              }
+            />
+          </div>
+        </div>
+        <div className="text-lg font-bold">
+          <div>
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
