4fbc8c1954effbb09f77655af658491ac52e135e
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-03 22:02:46 -0400

    Show 2 rows before show more
    hardcoded for now

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,15 @@
 interface ButtonProps {
   description: string;
   colour: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Button({ description, colour }: ButtonProps) {
+export default function Button({ description, colour, onClick }: ButtonProps) {
   return (
     <div>
-      <button className={`text-white px-4 py-2 rounded ${colour}`}>{description}</button>
+      <button onClick={onClick} className={`text-white px-4 py-2 rounded ${colour}`}>
+        {description}
+      </button>
     </div>
   );
 }
