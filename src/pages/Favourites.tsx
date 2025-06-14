4ea79a247aa6d18b21cde75f261cb2ac84744267
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-14 17:32:26 -0400

    default conditional rendering for favourites.tsx

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,26 +1,27 @@
 import { useMovieContext } from '../context/MovieContext';
 import MovieCard from '../components/MovieCard';
+import Banner from '../components/Banner';
 
 function Favourites() {
   const { favourites } = useMovieContext();
 
-  if (favourites) {
-    return (
-      <div>
-        <h2>Your Favourites</h2>
+  const isFavourites = favourites.length != 0;
+
+  return (
+    <div>
+      <h2>Your Favourites</h2>
+      {isFavourites ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
           {favourites.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
           ))}
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h2>No favourite movies yet</h2>
-      <p>Start adding movies to your favourites and they will appear here</p>
+      ) : (
+        <div>
+          <p> hi </p>
+          <Banner />
+        </div>
+      )}
     </div>
   );
 }
