c060ce43ec0e5c011a3b7d459a4ae426cf43586b
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-06-14 20:31:22 -0400

    render icons in banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,6 @@
+import { AlertTriangle, Info, CheckCircle } from 'lucide-react';
+import type { LucideIcon } from 'lucide-react';
+
 type BannerType = 'success' | 'warning' | 'info';
 
 type BannerProps = {
@@ -6,18 +9,38 @@ type BannerProps = {
   type: BannerType;
 };
 
-const bannerStyleMap: Record<BannerType, string> = {
-  success: 'bg-green-600 border-green-600',
-  warning: 'bg-yellow-400 border-yellow-600',
-  info: 'bg-blue-400 border-blue-600',
+type BannerConfig = {
+  color: string;
+  icon: LucideIcon;
+};
+
+const bannerStyleMap: Record<BannerType, BannerConfig> = {
+  success: {
+    color: 'bg-green-600 border-green-600',
+    icon: CheckCircle,
+  },
+  warning: {
+    color: 'bg-yellow-400 border-yellow-600',
+    icon: AlertTriangle,
+  },
+  info: {
+    color: 'bg-blue-400 border-blue-600',
+    icon: Info,
+  },
 };
 
 export default function Banner({ title, message, type }: BannerProps) {
-  const typeStyle = bannerStyleMap[type];
+  const { icon: Icon, color } = bannerStyleMap[type];
+
   return (
-    <div className={`py-6 rounded-lg border border-2 text-left px-4 ${typeStyle}`}>
-      <h1 className="text-xl"> {title} </h1>
-      <p> {message} </p>
+    <div
+      className={`py-6 rounded-lg border border-2 text-left px-4 flex items-start gap-4 ${color}`}
+    >
+      <Icon />
+      <div>
+        <h1 className="text-xl"> {title} </h1>
+        <p> {message} </p>
+      </div>
     </div>
   );
 }
